36f32e5d68570c52479a4b6e8f3b46a3032ddae3
Thyen-Radek <rabuhrabuhrabuh@poczta.fm>
2022-10-08 22:32:29 +0200

    Commit

--- lab1/oolab/src/main/java/agh/ics/oop/World.java
+++ lab1/oolab/src/main/java/agh/ics/oop/World.java
@@ -1,2 +1,45 @@
-package agh.ics.oop;public class World {
+package agh.ics.oop;
+
+public class World {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        String[] move = new String[]{"f","l","b","r"};
+        Direction[] directions = new Direction[move.length];
+        for (String argument : move){
+            Direction direction = switch (argument) {
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "l" -> Direction.LEFT;
+                case "r" -> Direction.RIGHT;
+                default -> Direction.DEFAULT;
+            };
+            directions[counter] = direction;
+            counter ++;
+        }
+        System.out.println("System wystartował");
+        run(directions);
+        System.out.println("System zakończył działanie");
+    }
+    public static void run(Direction[] arguments){
+        int counter = 0;
+//        System.out.println("Idzie w przód");
+//        System.out.println(arguments.length);
+        for (Direction argument : arguments){
+            counter ++;
+//            if (counter != arguments.length){
+//                argument += ",";
+//            }
+            String message = switch (argument) {
+                case FORWARD -> "Do przodu";
+                case BACKWARD -> "Do tyłu";
+                case LEFT -> "W lewo";
+                case RIGHT -> "W prawo";
+                default -> "Nieznana komenda";
+            };
+            System.out.println(message);
+
+        }
+    }
+
 }
