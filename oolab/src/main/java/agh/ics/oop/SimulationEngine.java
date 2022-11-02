5be768d94e0e96547b7f0a2d8ea74a788b887d91
Thyen-Radek <rabuhrabuhrabuh@poczta.fm>
2022-11-02 21:37:38 +0100

    Commit

--- oolab/src/main/java/agh/ics/oop/SimulationEngine.java
+++ oolab/src/main/java/agh/ics/oop/SimulationEngine.java
@@ -2,17 +2,17 @@ package agh.ics.oop;
 
 public class SimulationEngine implements IEngine{
     private final MoveDirection[] directions;
-    private final IWorldMap map;
+    private final IWorldMap map_animals;
     private int animals_count = 0;
     private final Animal[] animals;
-    public SimulationEngine(MoveDirection[] directions, IWorldMap map, Vector2d[] positions){
+    public SimulationEngine(MoveDirection[] directions, IWorldMap map_animals, Vector2d[] positions){
         this.directions = directions;
-        this.map = map;
+        this.map_animals = map_animals;
         this.animals = new Animal[positions.length];
         for (int i = 0;i<positions.length;i++) {
             boolean check;
-            Animal animal = new Animal(this.map, positions[i]);
-            check = this.map.place(animal);
+            Animal animal = new Animal(this.map_animals, positions[i]);
+            check = this.map_animals.place(animal);
             if (check){
                 this.animals[this.animals_count] = animal;
                 this.animals_count += 1;
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,12 +3,13 @@ public class World {
     public static void main(String[] args) {
         System.out.println("System wystartował");
         String[] move = new String[]{"f","b","r","l","f","f","r","r","f","f","f","f","f","f","f","f"};
-        MoveDirection[] directions = new OptionsParser().parse(move);
-        IWorldMap map = new RectangularMap(10, 5);
-        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
+        String[] move2 = new String[]{};
+        MoveDirection[] directions = new OptionsParser().parse(move2);
+        IWorldMap map = new GrassField(10);
+        Vector2d[] positions = { new Vector2d(1,2), new Vector2d(3,4) };
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
-        System.out.println(map.toString());
+        System.out.println(map);
         System.out.println("System zakończył działanie");
     }
     public static void run(MoveDirection[] arguments,Animal animal){
